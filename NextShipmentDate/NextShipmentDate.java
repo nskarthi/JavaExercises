fb5914a050f971a792d167a9df3566515faad643
Karthikeyan Selvaraj <nskarthi@gmail.com>
2021-03-30 16:55:39 -0400

    updated NextShipmentDate.java

--- NextShipmentDate/NextShipmentDate.java
+++ NextShipmentDate/NextShipmentDate.java
@@ -1,5 +1,6 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -10,13 +11,19 @@ public class NextShipmentDate {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		System.out.print("Recharged date: ");
 		Date rDate = sdf.parse(sc.nextLine());
+
 		System.out.print("Validity days: ");
 		int vDays = sc.nextInt();
 		sc.nextLine();
-		
+
 		System.out.println("Recharge Date: " + sdf.format(rDate));
 		System.out.println("Validity days: " + vDays);
 		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(rDate);
+		calendar.add(Calendar.DATE, vDays);
+		System.out.println("Next Recharge Date: " + sdf.format(calendar.getTime()));
+;
 		sc.close();
 	}
 
